e3e5b957acd111764256c74f1ceee61b658d4f81
Ian Flynn <himitsu.monban@gmail.com>
2017-09-03 17:03:33 -0400

    use async/await

--- popup/task.js
+++ popup/task.js
@@ -1,29 +1,29 @@
-function createTask(description) {
-  browser.storage.local.get('api_key').then((res) => {
-    if (res.api_key) {
-      let xhr = new XMLHttpRequest();
-      xhr.open('POST', 'https://inthe.am/api/v2/tasks/', true);
-      xhr.setRequestHeader("Authorization", "Token " + res.api_key);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send(JSON.stringify({"description": description}));
-    } else {
-      // Tell people they need to set up the extension?
-      console.log('api_key not set');
-    }
-  });
+async function createTask(description) {
+  const result = await browser.storage.local.get('api_key')
+  if (!result.api_key) {
+    console.log('api_key not set')
+    return
+  }
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://inthe.am/api/v2/tasks/', true);
+  xhr.setRequestHeader("Authorization", "Token " + result.api_key);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify({"description": description}));
 }
-document.getElementById('task_form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  let description = document.getElementById('task_description').value;
-  createTask(description);
-})
 
-document.addEventListener('DOMContentLoaded', (evt) => {
-  browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
-    let activeTab = tabs[0]
-    browser.tabs.sendMessage(activeTab.id, 'foo').then((res) => {
-      document.getElementById('task_description').value = res.data
-    })
+async function populateFields() {
+  const tabs = await browser.tabs.query({active: true, currentWindow: true})
+  const activeTab = tabs[0]
+  const res = await browser.tabs.sendMessage(activeTab.id, 'foo')
+  document.getElementById('task_description').value = res.data
+}
+
+document.addEventListener('DOMContentLoaded', function(evt) {
+  populateFields()
+  document.getElementById('task_form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let description = document.getElementById('task_description').value
+    createTask(description)
   })
 })
 

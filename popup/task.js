413c0305b7c3529cdaa8b8ff3fa05aacbe09377f
Ian Flynn <himitsu.monban@gmail.com>
2017-09-03 22:58:16 -0400

    provide feedback after submit

--- popup/task.js
+++ popup/task.js
@@ -5,10 +5,12 @@ async function createTask(description) {
     return
   }
   let xhr = new XMLHttpRequest();
+  xhr.addEventListener('loadend', showResult)
   xhr.open('POST', 'https://inthe.am/api/v2/tasks/', true);
   xhr.setRequestHeader("Authorization", "Token " + result.api_key);
   xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send(JSON.stringify({"description": description}));
+  showPending()
 }
 
 async function populateFields() {
@@ -16,6 +18,30 @@ async function populateFields() {
   const activeTab = tabs[0]
   const res = await browser.tabs.sendMessage(activeTab.id, 'foo')
   document.getElementById('task_description').value = res.data
+}
+
+function showResult(evt) {
+  const submit = document.getElementById('submit_button')
+  submit.value = 'Done'
+
+  let resultElement = document.createElement('p')
+  const xhr = evt.target
+  if (xhr.status == 200) {
+    resultElement.classList.add('success')
+    let resultText = document.createTextNode('Task created')
+    resultElement.appendChild(resultText)
+  } else {
+    resultElement.classList.add('error')
+    let resultText = document.createTextNode(xhr.statusText)
+    resultElement.appendChild(resultText)
+  }
+  document.body.appendChild(resultElement)
+}
+
+function showPending() {
+  const submit = document.getElementById('submit_button')
+  submit.disabled = true
+  submit.value = 'Sending...'
 }
 
 document.addEventListener('DOMContentLoaded', function(evt) {

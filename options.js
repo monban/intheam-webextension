708ea0576b932bb99baf2a0ba9eb1bec0fc47f8c
Ian Flynn <himitsu.monban@gmail.com>
2017-09-14 13:48:06 -0400

    ability to set project and tags of task

--- options.js
+++ options.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Load current settings from local store
-  browser.storage.local.get('api_key').then((res) => { 
+  browser.storage.local.get().then((res) => { 
     if (res.api_key)
       document.querySelector('#api_key').value = res.api_key
+    if (res.default_project)
+      document.querySelector('#default_project').value = res.default_project
+    if (res.default_tags)
+      document.querySelector('#default_tags').value = res.default_tags
   });
 
 
   // Override the form submit to store the settings
   document.querySelector('#optionsForm').addEventListener('submit', (evt) => {
     evt.preventDefault();
-    browser.storage.local.set({api_key: document.querySelector('#api_key').value});
+    const formdata = {
+      api_key: document.querySelector('#api_key').value,
+      default_project: document.querySelector('#default_project').value,
+      default_tags: document.querySelector('#default_tags').value
+    }
+      
+    browser.storage.local.set(formdata);
     // TODO Print some kind of message saying save succeeded
   });
 });
--- popup/task.js
+++ popup/task.js
@@ -1,4 +1,5 @@
-async function createTask(description) {
+async function createTask(taskdata) {
+  console.log('creating task: ', taskdata);
   const result = await browser.storage.local.get('api_key')
   if (!result.api_key) {
     console.log('api_key not set')
@@ -9,15 +10,22 @@ async function createTask(description) {
   xhr.open('POST', 'https://inthe.am/api/v2/tasks/', true);
   xhr.setRequestHeader("Authorization", "Token " + result.api_key);
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify({"description": description}));
+  xhr.send(JSON.stringify(taskdata));
   showPending()
 }
 
 async function populateFields() {
+  const defaults = await browser.storage.local.get(['default_project', 'default_tags'])
   const tabs = await browser.tabs.query({active: true, currentWindow: true})
   const activeTab = tabs[0]
   const res = await browser.tabs.sendMessage(activeTab.id, 'foo')
-  document.getElementById('task_description').value = res.data
+  document.querySelector('#task_description').value = res.data
+  if (defaults.default_project) {
+    document.querySelector('#task_project').value = defaults.default_project
+  }
+  if (defaults.default_tags) {
+    document.querySelector('#task_tags').value = defaults.default_tags
+  }
 }
 
 function showResult(evt) {
@@ -44,12 +52,18 @@ function showPending() {
   submit.value = 'Sending...'
 }
 
+function submitForm(evt) {
+    evt.preventDefault()
+    let taskdata = {
+      description: document.querySelector('#task_description').value,
+      project: document.querySelector('#task_project').value,
+      tags: document.querySelector('#task_tags').value.split(/[^\w]+/)
+    }
+    createTask(taskdata)
+}
+
 document.addEventListener('DOMContentLoaded', function(evt) {
   populateFields()
-  document.getElementById('task_form').addEventListener('submit', (e) => {
-    e.preventDefault()
-    let description = document.getElementById('task_description').value
-    createTask(description)
-  })
+  document.getElementById('task_form').addEventListener('submit', submitForm)
 })
 
